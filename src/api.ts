17405afa010522c654139def3db70b80f3645e92
Abdulhaq Emhemmed <el.ingeniero09@gmail.com>
2017-01-06 19:35:00 +0200

    Fix for urlbuilder

--- src/api.ts
+++ src/api.ts
@@ -86,151 +86,137 @@ export class NgrxJsonApi {
 
   public find(payload: Payload) {
 
+    let _generateIncludedQueryParams = generateIncludedQueryParams;
+    let _generateFilteringQueryParams = generateFilteringQueryParams;
+    let _generateFieldsQueryParams = generateFieldsQueryParams;
+    let _generateSortingQueryParams = generateSortingQueryParams;
+    let _generateQueryParams = generateQueryParams;
+
     if (this.config.hasOwnProperty('urlBuilder')) {
       let urlBuilder = this.config.urlBuilder;
-      let _generateIncludedQueryParams;
-      let _generateFilteringQueryParams;
-      let _generateFieldsQueryParams;
-      let _generateSortingQueryParams;
-      let _generateQueryParams;
 
       if (urlBuilder.generateIncludedQueryParams) {
         _generateIncludedQueryParams = urlBuilder.generateIncludedQueryParams;
-      } else {
-        _generateIncludedQueryParams = generateIncludedQueryParams;
       }
-
       if (urlBuilder.generateFilteringQueryParams) {
         _generateFilteringQueryParams = urlBuilder.generateFilteringQueryParams;
-      } else {
-        _generateFilteringQueryParams = generateFilteringQueryParams;
       }
-
       if (urlBuilder.generateFieldsQueryParams) {
         _generateFieldsQueryParams = urlBuilder.generateFieldsQueryParams;
-      } else {
-        _generateFieldsQueryParams = generateFieldsQueryParams;
       }
-
       if (urlBuilder.generateSortingQueryParams) {
         _generateSortingQueryParams = urlBuilder.generateSortingQueryParams;
-      } else {
-        _generateSortingQueryParams = generateSortingQueryParams;
       }
-
       if (urlBuilder.generateQueryParams) {
         _generateQueryParams = urlBuilder.generateQueryParams;
-      } else {
-        _generateQueryParams = generateQueryParams;
       }
-
-      let query = payload.query;
-      let queryParams = '';
-      let includedParam = '';
-      let filteringParams = '';
-      let sortingParams = '';
-      let fieldsParams = '';
-
-      if (typeof query === undefined) {
-        return Observable.throw('Query not found');
-      }
-
-      if (query.hasOwnProperty('params') && !_.isEmpty(query.params)) {
-        if (_.hasIn(query.params, 'include')) {
-          includedParam = _generateIncludedQueryParams(query.params.include);
-        }
-        if (_.hasIn(query.params, 'filtering')) {
-          filteringParams = _generateFilteringQueryParams(query.params.filtering);
-        }
-        if (_.hasIn(query.params, 'sorting')) {
-          sortingParams = _generateSortingQueryParams(query.params.sorting);
-        }
-        if (_.hasIn(query.params, 'fields')) {
-          fieldsParams = _generateFieldsQueryParams(query.params.fields);
-        }
-      }
-
-      queryParams = generateQueryParams(includedParam, filteringParams, sortingParams,
-        fieldsParams);
-
-      let requestOptionsArgs = {
-        method: RequestMethod.Get,
-        url: this.urlBuilder(query) + queryParams,
-      };
-
-      return this.request(requestOptionsArgs);
     }
-  }
+
+    let query = payload.query;
+    let queryParams = '';
+    let includedParam = '';
+    let filteringParams = '';
+    let sortingParams = '';
+    let fieldsParams = '';
+
+    if (typeof query === undefined) {
+      return Observable.throw('Query not found');
+    }
+
+    if (query.hasOwnProperty('params') && !_.isEmpty(query.params)) {
+      if (_.hasIn(query.params, 'include')) {
+        includedParam = _generateIncludedQueryParams(query.params.include);
+      }
+      if (_.hasIn(query.params, 'filtering')) {
+        filteringParams = _generateFilteringQueryParams(query.params.filtering);
+      }
+      if (_.hasIn(query.params, 'sorting')) {
+        sortingParams = _generateSortingQueryParams(query.params.sorting);
+      }
+      if (_.hasIn(query.params, 'fields')) {
+        fieldsParams = _generateFieldsQueryParams(query.params.fields);
+      }
+    }
+    queryParams = _generateQueryParams(includedParam, filteringParams, sortingParams,
+      fieldsParams);
+
+    let requestOptionsArgs = {
+      method: RequestMethod.Get,
+      url: this.urlBuilder(query) + queryParams,
+    };
+
+    return this.request(requestOptionsArgs);
+}
 
   public create(payload: Payload) {
 
-    let query = payload.query;
-    let document = payload.jsonApiData;
+  let query = payload.query;
+  let document = payload.jsonApiData;
 
-    if (typeof query === undefined) {
-      return Observable.throw('Query not found');
-    }
-
-    if (typeof document === undefined) {
-      return Observable.throw('Query not found');
-    }
-
-    let requestOptionsArgs = {
-      method: RequestMethod.Post,
-      url: this.urlBuilder(query),
-      body: JSON.stringify({ data: document.data })
-    };
-
-    return this.request(requestOptionsArgs);
+  if (typeof query === undefined) {
+    return Observable.throw('Query not found');
   }
+
+  if (typeof document === undefined) {
+    return Observable.throw('Query not found');
+  }
+
+  let requestOptionsArgs = {
+    method: RequestMethod.Post,
+    url: this.urlBuilder(query),
+    body: JSON.stringify({ data: document.data })
+  };
+
+  return this.request(requestOptionsArgs);
+}
 
   public update(payload: Payload) {
 
-    let query = payload.query;
-    let document = payload.jsonApiData;
+  let query = payload.query;
+  let document = payload.jsonApiData;
 
-    if (typeof query === undefined) {
-      return Observable.throw('Query not found');
-    }
-
-    if (typeof document === undefined) {
-      return Observable.throw('Query not found');
-    }
-    let requestOptionsArgs = {
-      method: RequestMethod.Patch,
-      url: this.urlBuilder(query),
-      body: JSON.stringify({ data: document.data })
-    };
-
-    return this.request(requestOptionsArgs);
+  if (typeof query === undefined) {
+    return Observable.throw('Query not found');
   }
 
-
-  public delete(payload: Payload) {
-
-    let query = payload.query;
-
-    if (typeof query === undefined) {
-      return Observable.throw('Query not found');
-    }
-
-    let requestOptions = {
-      method: RequestMethod.Delete,
-      url: this.urlBuilder(query)
-    };
-
-    return this.request(requestOptions);
+  if (typeof document === undefined) {
+    return Observable.throw('Query not found');
   }
+  let requestOptionsArgs = {
+    method: RequestMethod.Patch,
+    url: this.urlBuilder(query),
+    body: JSON.stringify({ data: document.data })
+  };
+
+  return this.request(requestOptionsArgs);
+}
+
+
+  public delete (payload: Payload) {
+
+  let query = payload.query;
+
+  if (typeof query === undefined) {
+    return Observable.throw('Query not found');
+  }
+
+  let requestOptions = {
+    method: RequestMethod.Delete,
+    url: this.urlBuilder(query)
+  };
+
+  return this.request(requestOptions);
+}
 
 
   private request(requestOptionsArgs) {
 
-    let requestOptions = new RequestOptions(requestOptionsArgs);
+  let requestOptions = new RequestOptions(requestOptionsArgs);
 
-    let request = new Request(requestOptions.merge({
-      headers: this.headers
-    }));
+  let request = new Request(requestOptions.merge({
+    headers: this.headers
+  }));
 
-    return this.http.request(request);
-  }
+  return this.http.request(request);
+}
 }
